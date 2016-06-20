544740369d0776fa832e786aecb66e8784a9e5ef
Chaoyang Liu <chaoyanglius@gmail.com>
2016-06-20 21:05:28 +0800

    This branch use node of interpolation produced by Chebyshev polynomial and it avoids runge's phenomenon.

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define N 40
+#define N 10
 #define a -5.0
 #define b 5.0
+
+#define PI 3.1415926535
 
 const double h = (b - a) / N;
 
@@ -18,9 +20,9 @@ double omega(double x, int k)
 {
     int i;
     double omega = 1.0, Xn;
-    for (i = 0; i < N; i++)
+    for (i = 0; i <= N; i++)
     {
-        Xn = a + i * h;
+        Xn = 5 * cos((21 - 2 * i) * PI / 22);
         if (i == k)
             continue;
         omega *= (x - Xn);
@@ -32,10 +34,10 @@ double omega_deri(int k)
 {
     int i;
     double omega_deri = 1.0, Xn;
-    double Xk = a + k * h;
-    for (i = 0; i < N; i++)
+    double Xk = 5 * cos((21 - 2 * k) * PI / 22);
+    for (i = 0; i <= N; i++)
     {
-        Xn = a + i * h;
+        Xn = 5 * cos((21 - 2 * i) * PI / 22);
         if (i == k)
             continue;
         omega_deri *= (Xk - Xn);
@@ -47,17 +49,25 @@ int main()
     double Lagra;
     double Xn, x;
     int i;
+    double j;
+    FILE *fp = fopen("result.txt", "w+");
 
-    printf("Please enter x: ");
-    scanf("%lf", &x);
-
-    for (i = 0; i < N; i++)
+    //printf("Please enter x: ");
+    //scanf("%lf", &x);
+    x = -4.0;
+    for (j = a; j < b; j += 0.1)
     {
-        Xn = a + i * h;
+    x = j;
+    for (i = 0; i <= N; i++)
+    {
+        Xn = 5 * cos((21 - 2 * i) * PI / 22);
         Lagra += ((func(Xn) * omega(x, i)) / omega_deri(i));
     }
-
-    printf("The approximate value is: %lf\n", Lagra);
-    printf("The real value is: %lf\n", func(x));
+    //printf("The approximate value is: %lf\n", Lagra);
+    //printf("The real value is: %lf\n", func(x));
+    fprintf(fp, "%lf\t%lf\t%lf\n", x, Lagra, func(x));
+    Lagra = 0.0;
+    }
+    fclose(fp);
     return 0;
 }
